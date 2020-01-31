aba57cd346e2aec6f678b69644aa61a40b7a0467
Alex Coleman <alexjcoleman@gmail.com>
2020-01-31 14:30:21 +0000

    updated conda settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,7 @@ apt-get install git
 wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b -p /home/vagrant/miniconda
-echo 'export PATH="/home/vagrant/miniconda/bin:$PATH"' >> /home/vagrant/.bashrc
-source /home/vagrant/.bashrc
+source /home/vagrant/miniconda/etc/profile.d/conda.sh
 chown -R vagrant:vagrant /home/vagrant/miniconda
 
 # setting up ssh forwarding
@@ -20,7 +19,7 @@ chown -R vagrant:vagrant /home/vagrant/miniconda
 
 git clone https://github.com/voila-gallery/hello-world-example.git
 cd hello-world-example
-/home/vagrant/miniconda/bin/conda env create -f environment.yml
-/home/vagrant/miniconda/bin/conda activate voila-gallery-hello-world-example
+conda env create -f environment.yml
+conda activate voila-hello-world-example
 
-voila --port=4567 app.ipynb
+voila --port=4567 --no-browser app.ipynb
